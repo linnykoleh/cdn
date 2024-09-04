c26d10b193a0351ca09864b2a89da41c793689fc
linnykoleh <oleh.linnyk@megogo.net>
2024-09-04 14:46:04 +0300

    Initial commit

---
+++ src/main/java/demo/cdn/CdnApplication.java
@@ -0,0 +1,13 @@
+package demo.cdn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/cdn/CdnApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.cdn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
